06cfe88a6d2e82cc6269f84ca601597f5f87c756
Dipesh Kumar <dipeshkumar@microsoft.com>
2024-11-22 00:05:46 +0530

    Adding Fluent UnitTests

---
+++ src/Microsoft.DotNet.Wpf/tests/UnitTests/PresentationFramework.Fluent.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Windows.Controls;
+
+namespace PresentationFramework.Fluent.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Button button = new Button();
+
+    }
+}
